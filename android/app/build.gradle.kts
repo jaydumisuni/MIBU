f5e1969bd0d61010aad7ec90699c3d538ee15088
THE TECH GUY <65334711+jaydumisuni@users.noreply.github.com>
2026-06-20 21:01:10 +0200

    Add MIBU Android app module

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.thetechguy.mibu"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.thetechguy.mibu"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0-dev"
+    }
+}
